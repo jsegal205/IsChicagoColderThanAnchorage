ec9c16dd6d2b5a13bc33e088c8b515d35af41f77
Jim Segal Macbook <jsegal205@gmail.com>
2015-01-05 23:23:04 -0600

    whoops remove api key.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 	function getChicagoTemp(){
 		$.ajax({
 			type:'POST',
-			url:'https://api.forecast.io/forecast/beccc175da5b3e505d9eeb449c5a3222/41.8369,-87.6847',
+			url:'https://api.forecast.io/forecast/xxxxxxxxxxxxxxxxxxxxx/41.8369,-87.6847',
 			dataType: 'jsonp',
 			success:function(data){
 				$chicago.text(data.currently.temperature);
@@ -18,7 +18,7 @@
 	function getAnchTemp(){
 		$.ajax({
 			type:'POST',
-			url:'https://api.forecast.io/forecast/beccc175da5b3e505d9eeb449c5a3222/61.2175,-149.8584',
+			url:'https://api.forecast.io/forecast/xxxxxxxxxxxxxxxxxxxxx/61.2175,-149.8584',
 			dataType: 'jsonp',
 			success:function(data){
 				$anch.text(data.currently.temperature);
